92b9bc3d4a1f9fc8dd6cb7f141f484f708527c2a
matt257 <mattwalton257@gmail.com>
2024-04-24 21:21:02 -0400

    added startup.sh for ondemand, working on dex

---
+++ ondemand/startup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Start ondemand-dex in the background
+runuser -u ondemand-dex -- /usr/sbin/ondemand-dex serve /etc/ood/dex/config.yaml &
+
+# Start httpd in the foreground
+exec /usr/sbin/httpd -D FOREGROUND
