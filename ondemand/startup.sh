581e37dce5c2b25f37c311780a1ff7f31f739cdc
matt257 <mattwalton257@gmail.com>
2024-04-28 17:43:30 -0400

    more ldap changes

--- ondemand/startup.sh
+++ ondemand/startup.sh
@@ -2,6 +2,5 @@
 
 # Start ondemand-dex in the background
 runuser -u ondemand-dex -- /usr/sbin/ondemand-dex serve /etc/ood/dex/config.yaml &
-
 # Start httpd in the foreground
 exec /usr/sbin/httpd -D FOREGROUND
